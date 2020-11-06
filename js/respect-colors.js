0a65518420704bc3446b6430b2f426fc086257aa
Edward-Alarco <edwarddac.56@gmail.com>
2020-11-06 17:50:26 -0600

    Terminando estructura y estilos w/ scss

---
+++ js/respect-colors.js
@@ -0,0 +1,112 @@
+(function(){
+
+
+    document.getElementsByClassName('columna-of-a')[0].firstElementChild.style.background = 'linear-gradient(#ff1000, #fb39b9)'
+    document.getElementsByClassName('columna-of-a')[1].firstElementChild.style.background = 'linear-gradient(#ff1000, #c9f72a)'
+    document.getElementsByClassName('columna-of-a')[2].firstElementChild.style.background = 'linear-gradient(#ff1000, #fb852e)'
+    document.getElementsByClassName('columna-of-a')[3].firstElementChild.style.background = 'linear-gradient(#ff1000, #f1c40f)'
+    document.getElementsByClassName('columna-of-a')[4].firstElementChild.style.background = 'linear-gradient(#ff1000, #e5c089)'
+    document.getElementsByClassName('columna-of-a')[5].firstElementChild.style.background = 'linear-gradient(#ff1000, #b533f8)'
+    document.getElementsByClassName('columna-of-a')[6].firstElementChild.style.background = 'linear-gradient(#ff1000, #ffdd59)'
+
+
+    document.getElementsByClassName('columna-of-b')[0].firstElementChild.style.background = 'linear-gradient(#ff5200, #fb852e)'
+    document.getElementsByClassName('columna-of-b')[1].firstElementChild.style.background = 'linear-gradient(#ff5200, #ffdd59)'
+    document.getElementsByClassName('columna-of-b')[2].firstElementChild.style.background = 'linear-gradient(#ff5200, #fb39b9)'
+    document.getElementsByClassName('columna-of-b')[3].firstElementChild.style.background = 'linear-gradient(#ff5200, #f1c40f)'
+    document.getElementsByClassName('columna-of-b')[4].firstElementChild.style.background = 'linear-gradient(#ff5200, #b533f8)'
+    document.getElementsByClassName('columna-of-b')[5].firstElementChild.style.background = 'linear-gradient(#ff5200, #e5c089)'
+    document.getElementsByClassName('columna-of-b')[6].firstElementChild.style.background = 'linear-gradient(#ff5200, #c9f72a)'
+
+
+    document.getElementsByClassName('columna-of-c')[0].firstElementChild.style.background = 'linear-gradient(#ff9c00, #fb852e)'
+    document.getElementsByClassName('columna-of-c')[1].firstElementChild.style.background = 'linear-gradient(#ff9c00, #ffdd59)'
+    document.getElementsByClassName('columna-of-c')[2].firstElementChild.style.background = 'linear-gradient(#ff9c00, #fb39b9)'
+    document.getElementsByClassName('columna-of-c')[3].firstElementChild.style.background = 'linear-gradient(#ff9c00, #f1c40f)'
+    document.getElementsByClassName('columna-of-c')[4].firstElementChild.style.background = 'linear-gradient(#ff9c00, #b533f8)'
+    document.getElementsByClassName('columna-of-c')[5].firstElementChild.style.background = 'linear-gradient(#ff9c00, #e5c089)'
+    document.getElementsByClassName('columna-of-c')[6].firstElementChild.style.background = 'linear-gradient(#ff9c00, #c9f72a)'
+
+
+    document.getElementsByClassName('columna-of-d')[0].firstElementChild.style.background = 'linear-gradient(#ffc600, #ff675b)'
+    document.getElementsByClassName('columna-of-d')[1].firstElementChild.style.background = 'linear-gradient(#ffc600, #f1bb87)'
+    document.getElementsByClassName('columna-of-d')[2].firstElementChild.style.background = 'linear-gradient(#ffc600, #f78e69)'
+    document.getElementsByClassName('columna-of-d')[3].firstElementChild.style.background = 'linear-gradient(#ffc600, #dd5d26)'
+    document.getElementsByClassName('columna-of-d')[4].firstElementChild.style.background = 'linear-gradient(#ffc600, #ef7f3f)'
+    document.getElementsByClassName('columna-of-d')[5].firstElementChild.style.background = 'linear-gradient(#ffc600, #ff9000)'
+    document.getElementsByClassName('columna-of-d')[6].firstElementChild.style.background = 'linear-gradient(#ffc600, #dc5c05)'
+
+
+    document.getElementsByClassName('columna-of-e')[0].firstElementChild.style.background = 'linear-gradient(#fbeb00, #fdba03)'
+    document.getElementsByClassName('columna-of-e')[1].firstElementChild.style.background = 'linear-gradient(#fbeb00, #fd6d03)'
+    document.getElementsByClassName('columna-of-e')[2].firstElementChild.style.background = 'linear-gradient(#fbeb00, #fdba03)'
+    document.getElementsByClassName('columna-of-e')[3].firstElementChild.style.background = 'linear-gradient(#fbeb00, #c5fd03)'
+    document.getElementsByClassName('columna-of-e')[4].firstElementChild.style.background = 'linear-gradient(#fbeb00, #E4A010)'
+    document.getElementsByClassName('columna-of-e')[5].firstElementChild.style.background = 'linear-gradient(#fbeb00, #c6ce00)'
+    document.getElementsByClassName('columna-of-e')[6].firstElementChild.style.background = 'linear-gradient(#fbeb00, #ffd55a)'
+
+
+    document.getElementsByClassName('columna-of-f')[0].firstElementChild.style.background = 'linear-gradient(#deff00, #d5eb96)'
+    document.getElementsByClassName('columna-of-f')[1].firstElementChild.style.background = 'linear-gradient(#deff00, #5ca957)'
+    document.getElementsByClassName('columna-of-f')[2].firstElementChild.style.background = 'linear-gradient(#deff00, #2e5857)'
+    document.getElementsByClassName('columna-of-f')[3].firstElementChild.style.background = 'linear-gradient(#deff00, #02ff8a)'
+    document.getElementsByClassName('columna-of-f')[4].firstElementChild.style.background = 'linear-gradient(#deff00, #ffe641)'
+    document.getElementsByClassName('columna-of-f')[5].firstElementChild.style.background = 'linear-gradient(#deff00, #3cfe00)'
+    document.getElementsByClassName('columna-of-f')[6].firstElementChild.style.background = 'linear-gradient(#deff00, #219729)'
+
+
+    document.getElementsByClassName('columna-of-g')[0].firstElementChild.style.background = 'linear-gradient(#18ff06, #01b673)'
+    document.getElementsByClassName('columna-of-g')[1].firstElementChild.style.background = 'linear-gradient(#18ff06, #418c77)'
+    document.getElementsByClassName('columna-of-g')[2].firstElementChild.style.background = 'linear-gradient(#18ff06, #8eb775)'
+    document.getElementsByClassName('columna-of-g')[3].firstElementChild.style.background = 'linear-gradient(#18ff06, #34643e)'
+    document.getElementsByClassName('columna-of-g')[4].firstElementChild.style.background = 'linear-gradient(#18ff06, #3ef5e1)'
+    document.getElementsByClassName('columna-of-g')[5].firstElementChild.style.background = 'linear-gradient(#18ff06, #91a464)'
+    document.getElementsByClassName('columna-of-g')[6].firstElementChild.style.background = 'linear-gradient(#18ff06, #55d983)'
+
+
+    document.getElementsByClassName('columna-of-h')[0].firstElementChild.style.background = 'linear-gradient(#00a08c, #5dc1b9)'
+    document.getElementsByClassName('columna-of-h')[1].firstElementChild.style.background = 'linear-gradient(#00a08c, #5497a2)'
+    document.getElementsByClassName('columna-of-h')[2].firstElementChild.style.background = 'linear-gradient(#00a08c, #2ca880)'
+    document.getElementsByClassName('columna-of-h')[3].firstElementChild.style.background = 'linear-gradient(#00a08c, #72c8c1)'
+    document.getElementsByClassName('columna-of-h')[4].firstElementChild.style.background = 'linear-gradient(#00a08c, #00986c)'
+    document.getElementsByClassName('columna-of-h')[5].firstElementChild.style.background = 'linear-gradient(#00a08c, #3fa59e)'
+    document.getElementsByClassName('columna-of-h')[6].firstElementChild.style.background = 'linear-gradient(#00a08c, #239089)'
+
+
+    document.getElementsByClassName('columna-of-i')[0].firstElementChild.style.background = 'linear-gradient(#004aff, #03e3fd)'
+    document.getElementsByClassName('columna-of-i')[1].firstElementChild.style.background = 'linear-gradient(#004aff, #77a8b7)'
+    document.getElementsByClassName('columna-of-i')[2].firstElementChild.style.background = 'linear-gradient(#004aff, #6d03fd)'
+    document.getElementsByClassName('columna-of-i')[3].firstElementChild.style.background = 'linear-gradient(#004aff, #03e3fd)'
+    document.getElementsByClassName('columna-of-i')[4].firstElementChild.style.background = 'linear-gradient(#004aff, #3498db)'
+    document.getElementsByClassName('columna-of-i')[5].firstElementChild.style.background = 'linear-gradient(#004aff, #20547b)'
+    document.getElementsByClassName('columna-of-i')[6].firstElementChild.style.background = 'linear-gradient(#004aff, #8cd7ae)'
+
+
+    document.getElementsByClassName('columna-of-j')[0].firstElementChild.style.background = 'linear-gradient(#8408ff, #d455ae)'
+    document.getElementsByClassName('columna-of-j')[1].firstElementChild.style.background = 'linear-gradient(#8408ff, #962dae)'
+    document.getElementsByClassName('columna-of-j')[2].firstElementChild.style.background = 'linear-gradient(#8408ff, #ca7fde)'
+    document.getElementsByClassName('columna-of-j')[3].firstElementChild.style.background = 'linear-gradient(#8408ff, #55d3bb)'
+    document.getElementsByClassName('columna-of-j')[4].firstElementChild.style.background = 'linear-gradient(#8408ff, #94009c)'
+    document.getElementsByClassName('columna-of-j')[5].firstElementChild.style.background = 'linear-gradient(#8408ff, #ba55d3)'
+    document.getElementsByClassName('columna-of-j')[6].firstElementChild.style.background = 'linear-gradient(#8408ff, #9497ca)'
+
+
+    document.getElementsByClassName('columna-of-k')[0].firstElementChild.style.background = 'linear-gradient(#94009c, #ffff86)'
+    document.getElementsByClassName('columna-of-k')[1].firstElementChild.style.background = 'linear-gradient(#94009c, #8a1a9f)'
+    document.getElementsByClassName('columna-of-k')[2].firstElementChild.style.background = 'linear-gradient(#94009c, #4a1942)'
+    document.getElementsByClassName('columna-of-k')[3].firstElementChild.style.background = 'linear-gradient(#94009c, #9c7da9)'
+    document.getElementsByClassName('columna-of-k')[4].firstElementChild.style.background = 'linear-gradient(#94009c, #3d0b60)'
+    document.getElementsByClassName('columna-of-k')[5].firstElementChild.style.background = 'linear-gradient(#94009c, #5e2d44)'
+    document.getElementsByClassName('columna-of-k')[6].firstElementChild.style.background = 'linear-gradient(#94009c, #ba55d3)'
+
+
+    document.getElementsByClassName('columna-of-l')[0].firstElementChild.style.background = 'linear-gradient(#bd006b, #5f27cd)'
+    document.getElementsByClassName('columna-of-l')[1].firstElementChild.style.background = 'linear-gradient(#bd006b, #16a085)'
+    document.getElementsByClassName('columna-of-l')[2].firstElementChild.style.background = 'linear-gradient(#bd006b, #fb39b9)'
+    document.getElementsByClassName('columna-of-l')[3].firstElementChild.style.background = 'linear-gradient(#bd006b, #9b59b6)'
+    document.getElementsByClassName('columna-of-l')[4].firstElementChild.style.background = 'linear-gradient(#bd006b, #ff9ff3)'
+    document.getElementsByClassName('columna-of-l')[5].firstElementChild.style.background = 'linear-gradient(#bd006b, #e5c089)'
+    document.getElementsByClassName('columna-of-l')[6].firstElementChild.style.background = 'linear-gradient(#bd006b, #c9f72a)'
+
+
+}())
