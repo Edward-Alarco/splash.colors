4134c8b49b929f9ff2f9f0307a689a8b5949c50e
Edward-Alarco <edwarddac.56@gmail.com>
2020-11-07 09:57:54 -0600

    to ghpages

--- js/modal.js
+++ js/modal.js
@@ -10,3 +10,14 @@ buttonActivator.addEventListener('click',function(){
 closebtn.addEventListener('click',function(){
     modal.style.display = 'none'
 })
+
+const color = document.getElementsByClassName('buttonColorContenedor')
+const modalCopy = document.getElementsByClassName('copiado')[0]
+
+
+for (let i = 0; i < color.length; i++) {
+    color[i].addEventListener('click',function(){
+        modalCopy.style.display = 'flex'
+        setTimeout(()=>modalCopy.style.display = 'none',2000)
+    })
+}
